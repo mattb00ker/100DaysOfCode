d6a309394a2e99f86214416e3e6b20b86eacfa2b
mattb00ker <73802648+mattb00ker@users.noreply.github.com>
2022-04-11 23:39:52 +0100

    first code added

---
+++ followerAnalyser.py
@@ -0,0 +1,28 @@
+# Import tweepy library
+
+import tweepy
+
+bearer_token = ""
+
+client = tweepy.Client(bearer_token)
+
+# Get User's Followers
+
+# This endpoint/method returns a list of users who are followers of the
+# specified user ID
+
+user_id = 1022803686274334720
+
+# By default, only the ID, name, and username fields of each user will be
+# returned
+# Additional fields can be retrieved using the user_fields parameter
+response = client.get_users_followers(
+    user_id, user_fields=["profile_image_url"]
+)
+
+for user in response.data:
+    print(user.username)
+
+# By default, this endpoint/method returns 100 results
+# You can retrieve up to 1000 users by specifying max_results
+response = client.get_users_followers(user_id, max_results=100) 
