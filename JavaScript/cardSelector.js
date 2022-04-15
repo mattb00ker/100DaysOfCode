e0663e28bad48f6822060e8de3a6055c542be8b1
mattb00ker <73802648+mattb00ker@users.noreply.github.com>
2022-04-15 22:22:54 +0100

    added switch case and two players

--- JavaScript/cardSelector.js
+++ JavaScript/cardSelector.js
@@ -4,13 +4,59 @@
 
 var cards = new Array("AH", "AD", "AC", "AS");
 
+//Initialise "first card"
+
+var cardOne;
+
+// Initialise "second card"
+
+var cardTwo;
+
+// Build ranking system
+
+var rank;
+
+
+
 function getRandomCard(){
     var choice = Math.floor(Math.random()*(cards.length));
-    console.log(choice);
-    console.log(cards[choice]);
+    //console.log(choice);
+    //console.log(cards[choice]);
     
     return cards[choice];
-    
-
 }
 
+function cardValue(card){
+
+    console.log(card);
+
+    switch(card){
+        case "AH":
+            rank =0;
+            console.log(rank); 
+            break;
+         case "AD":
+            rank =1;
+            console.log(rank); 
+            break;
+        case "AS":
+            rank =2;
+            console.log(rank); 
+            break;
+        case "AC":
+            rank = 3;
+            console.log(rank); 
+            break;
+    }
+    
+}
+
+function pickCard(){
+    cardTwo = cardOne;
+
+    cardOne = getRandomCard();
+
+    console.log("cardOne: ", cardOne);
+    console.log("cardTwo: ", cardTwo);
+
+}
