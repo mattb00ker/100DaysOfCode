0311ce0fc9adb8c30861148281f9900f95a8471e
Matt Booker <73802648+mattb00ker@users.noreply.github.com>
2023-04-23 13:17:51 +0100

    updated game file

--- shitHead game/karma.js
+++ shitHead game/karma.js
@@ -22,15 +22,92 @@ function drawCard(deck) {
   return deck.pop();
 }
 
-// Main game logic
-const deck = createDeck();
-shuffleDeck(deck);
-const playerHand = [];
+function updatePlayerHandDisplay(playerHand) {
+  const playerHandElement = document.getElementById('player-hand');
+  playerHandElement.innerHTML = '';
 
-document.getElementById('draw-card').addEventListener('click', () => {
-  const drawnCard = drawCard(deck);
-  playerHand.push(drawnCard);
-  updatePlayerHandDisplay(playerHand);
-});
+  for (let card of playerHand) {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.textContent = card.value;
+    cardElement.dataset.suit = card.suit;
+    cardElement.dataset.value = card.value;
+    playerHandElement.appendChild(cardElement);
+  }
+}
 
-// You'll need to add functions for playing cards, managing discard piles, and determining the winner.
+let requiredDirection = 'higher'; // Possible values: 'higher', 'lower'
+
+function canPlayCard(card, topCard) {
+  if (card.value === '2' || card.value === '8' || card.value === '9' || card.value === '10') {
+    return true;
+  }
+
+  const cardValueIndex = values.indexOf(card.value);
+  const topCardValueIndex = values.indexOf(topCard.value);
+
+  if (topCard.value === '8') {
+    return true;
+  }
+
+  if (requiredDirection === 'higher') {
+    return cardValueIndex > topCardValueIndex;
+  } else {
+    return cardValueIndex < topCardValueIndex;
+  }
+}
+
+function checkForWinner(playerHand) {
+  if (playerHand.length === 0) {
+    return true;
+  }
+  return false;
+}
+
+function playCard(playerHand, cardIndex, discardPile) {
+  const playedCard = playerHand[cardIndex];
+
+  if (discardPile.length > 0) {
+    const topCard = discardPile[discardPile.length - 1];
+
+    if (!canPlayCard(playedCard, topCard)) {
+      alert('You cannot play this card on the current discard pile.');
+      return;
+    }
+  }
+
+  playerHand.splice(cardIndex, 1);
+  discardPile.push(playedCard);
+
+  if (playedCard.value === '2') {
+    requiredDirection = 'higher';
+  } else if (playedCard.value === '9') {
+    requiredDirection = prompt('You played a 9. Choose the required direction for the next player (higher/lower):').toLowerCase();
+  } else if (playedCard.value === '10') {
+    discardPile.length = 0;
+  }
+
+  // After playing a card, check for a winner.
+  if (checkForWinner(playerHand)) {
+    alert('Congratulations, you have won the game!');
+    // You can reset the game or provide an option to restart the game here.
+  }
+}
+
+function updateDiscardPilesDisplay(discardPiles) {
+  discardPiles.forEach((discardPile, index) => {
+    const discardPileElement = document.getElementById(`discard-pile-${index + 1}`);
+    const topCard = discardPile[discardPile.length - 1];
+
+    if (topCard) {
+      discardPileElement.textContent = topCard.value;
+      discardPileElement.dataset.suit = topCard.suit;
+      discardPileElement.dataset.value = topCard.value;
+    } else {
+      discardPileElement.textContent = '';
+      discardPileElement.removeAttribute('data-suit');
+      discardPileElement.removeAttribute('data-value');
+    }
+  });
+}
+
